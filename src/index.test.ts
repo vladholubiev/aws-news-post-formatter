ced96d2e7b8c3e0b3d3502747b380dba51f3fd5a
Vlad Holubiev <golubev.vld@gmail.com>
2023-03-06 14:06:47 +0100

    feat: skeleton setup with presets

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,18 @@
+import 'expect-puppeteer';
 import {getFoo} from './index';
 
 it('should return bar', () => {
   expect(getFoo()).toEqual('bar');
+});
+
+describe('google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    const image = await page.screenshot({type: 'png'});
+
+    expect(image).toMatchImageSnapshot();
+  });
 });
---
+++ types-3rd-party/types.ts
@@ -0,0 +1,5 @@
+import type {Page} from 'puppeteer-core';
+
+declare global {
+  type page = Page;
+}
