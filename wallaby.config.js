532b3604097ac907ef5e1328e1f6e8ba7b2edd36
Vlad Holubiev <golubev.vld@gmail.com>
2023-03-18 19:18:21 +0100

    chore: add wallaby config for running test-related tests

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,12 +1,17 @@
 module.exports = () => {
   return {
     autoDetect: true,
-    files: ['package.json', 'src/**/*.ts', '!src/**/*.test.ts'],
-    tests: ['src/**/*.test.ts'],
+    files: ['package.json', 'src/**/get-custom-tags.ts', '!src/**/get-custom-tags.test.ts'],
+    tests: ['src/**/get-custom-tags.test.ts'],
     env: {
       params: {
         env: 'TZ=UTC',
       },
+    },
+    testFramework: {
+      // the jest configuration file path
+      // (relative to project root)
+      configFile: './jest.config.wallaby.js',
     },
   };
 };
