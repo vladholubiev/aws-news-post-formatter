91190af279c8f8a88818706f5bfaa8bfca616d2f
Vlad Holubiev <golubev.vld@gmail.com>
2023-09-25 12:43:50 +0200

    feat: add sap tag rules

--- src/custom-tags/sap.ts
+++ src/custom-tags/sap.ts
@@ -3,11 +3,13 @@ export default [
     headlineIncludes: [
       'SAP HANA',
       'S/4HANA',
+      'SAP®',
       'HANA based SAP',
       'SAP NetWeaver',
       'SAP ABAP',
       'SAP application',
       'SAP deployment',
+      'SAP workload',
     ],
   },
 ];
